6ca9e541e0d0090025a8655fee7fcdf3d8bf1c86
adrian-azan <adrian.azan@hotmail.com>
2023-09-29 23:09:25 -0400

    WOW
    Actually proud of myself for my speed. I decided to try mocking rocket slime battle this morning and I've already got a pretty decent first pass of the middle and lower deck plus shoots! Helps to have a flow between blender godot and material maker now. Will try to improve the dash next

---
+++ Code/Player.cs
@@ -0,0 +1,109 @@
+
+using Godot;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+
+public partial class Player : Node3D
+{
+	private RigidBody3D _rigidBody;
+	private float _acceleration = 50f;
+	private float _speed = 4f;
+  public override void _PhysicsProcess(double delta)
+{
+	Vector3 velocity = new Vector3(0, 0, 0);
+
+	
+		if (Input.IsActionPressed("MoveForward"))
+		{
+			velocity.Z -= 1.0f; // Move forward
+		}
+		if (Input.IsActionPressed("MoveBackward"))
+		{
+			velocity.Z += 1.0f; // Move backward
+		}
+		if (Input.IsActionPressed("MoveLeft"))
+		{
+			velocity.X += 1.0f; // Move left
+		}
+		if (Input.IsActionPressed("MoveRight"))
+		{
+			velocity.X -= 1.0f; // Move right
+		}
+
+		// Normalize the velocity vector to ensure consistent speed when moving diagonally.
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * _acceleration;
+		}
+
+		if (Input.IsActionJustReleased("Dash"))
+		{
+            velocity *= 3.0f; // Jump
+			Dash();
+        }
+	
+	 _rigidBody.ApplyCentralForce(velocity);
+
+	//TODO: confirm why linearvelocity is never 0
+	_rigidBody.LinearVelocity = _rigidBody.LinearVelocity.LimitLength(_speed);
+
+}
+
+	public async void Dash()
+	{
+		_speed *= 2;
+		await ToSignal(GetTree().CreateTimer(1.0f), SceneTreeTimer.SignalName.Timeout);		
+		_speed /= 2;
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_rigidBody = GetNode<RigidBody3D>("RigidBody3D");
+		GD.Print(_rigidBody);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+
+
+public void Check(bool state)
+{
+
+		for (int i = 0; i < GetChildCount(); i++)
+		{
+            var child = GetChild<Node3D>(i);
+			GD.Print(child.Name);
+            if (child.Name == "Exclamation")
+			{
+                child.Visible = state;
+            }
+			else
+			{
+				for (int j = 0; j < child.GetChildCount(); j++)
+				{
+					var grandchild = child.GetChild<Node3D>(j);
+					GD.Print(grandchild.Name);
+					if (grandchild.Name == "Exclamation")
+					{
+						grandchild.Visible = state;
+					}
+				}
+			}
+        }		
+}
+
+
+
+}
+
+
+
+
+
+
+
---
+++ Code/promptable.cs
@@ -0,0 +1,62 @@
+using Godot;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+public partial class promptable : Node
+{
+    [ExportGroup("Prompt")]
+    [Export(PropertyHint.File)]
+    public Resource JsonFile;
+
+    public Node3D _Accent;
+
+    bool _isBeingPercieved = false;
+    public override void _Process(double delta)
+	{
+       if (_isBeingPercieved && Input.IsActionJustPressed("Interact"))
+		{
+            //FilePath.
+            var file = FileAccess.Open(JsonFile.ResourcePath, FileAccess.ModeFlags.Read);
+             var content = Json.ParseString(file.GetAsText());
+            GD.Print(content.AsGodotDictionary()["details"]);
+
+           // GD.Print(FilePath.()["details"]);
+		}
+       
+       
+	}
+
+    public void BeingPercieved(Node3D other)
+    {
+        var target = other.GetOwnerOrNull<Node3D>();
+
+        if (target.Name == "Player")
+        {
+            _isBeingPercieved = true;
+            var  test = (Player)target;
+            test.Check(true);
+            //GD.Print("PLAYER {}",other);    
+        }
+
+    }
+      public void NotBeingPercieved(Node3D other)
+    {
+        var target = other.GetOwnerOrNull<Node3D>();
+
+        if (target.Name == "Player")
+        {         
+            _isBeingPercieved = false;
+                        var  test = (Player)target;
+
+                        test.Check(false);
+
+            //GD.Print("PLAYER {}",other);    
+        }
+   
+    }
+
+    public override string ToString()
+    {
+        return "aass";//file.GetParsedText();
+    }
+}
